818f980b65905410d983f34472a6abd6d78cc97e
Kendrick Tan <kendricktan0814@gmail.com>
2016-08-28 02:05:33 +1000

    forest works

--- checker/views.py
+++ checker/views.py
@@ -1,30 +1,29 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from checker.forms import ImageForm
+import checker.forest as forest
 
 from skimage import io
-from checker.predict import predict, WIDTH, HEIGHT, DEPTH, json_settings
 
 import cv2
 import numpy as np
+
+# Good RGB for 
+
+def ColorDistance(rgb1,rgb2):
+    '''d = {} distance between two colors(3)'''
+    rm = 0.5*(rgb1[0]+rgb2[0])
+    d = sum((2+rm,4,3-rm)*(rgb1-rgb2)**2)**0.5
+    return d
 
 # Create your views here.
 def index_view(request):
     if request.method == 'POST':        
         url = request.POST['image_url']                
-        image = io.imread(url)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = cv2.resize(image, (WIDTH, HEIGHT), interpolation=cv2.INTER_CUBIC)
-        image = np.reshape(image, (1, HEIGHT, WIDTH, DEPTH))
-
+        ctx = forest.predict(url)      
+        
         # Raw values
-        results_raw = predict(image)
-        print(results_raw)
-        results_index = np.argmax(results_raw)
-        results_score = results_raw[0][results_index]*100
-        results_str = json_settings['level'][str(results_index)]
-        context = {'type': results_str, 'results_score': results_score}
-        return JsonResponse(context)            
+        return JsonResponse(ctx)            
 
     form = ImageForm()
     return render(request, 'index.html', {'form': form})
---
+++ forest.py
@@ -0,0 +1,68 @@
+from skimage import io
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import cross_validation
+from sklearn.svm import SVC
+
+import cv2, glob
+import numpy as np
+
+# Get normalized histogram
+def describe(image, mask = None):
+    hist = cv2.calcHist([image], [0, 1, 2], mask, [8,8,8], [0, 256, 0, 256, 0, 256])    
+    cv2.normalize(hist, hist)    
+    return hist.flatten()
+
+data = []
+target = []
+
+# Sort through images
+image_paths = sorted(glob.glob("meat/*.jpg"))
+for image_path in image_paths:    
+    # Read image
+    image = io.imread(image_path)    
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)    
+    image = cv2.resize(image, (500, 500), interpolation=cv2.INTER_CUBIC)    
+
+    # Use otsu to mask
+    gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    mask = cv2.medianBlur(mask, 5)
+
+    features = describe(image, mask=mask)    
+
+    # Append to dataset
+    data.append(features)
+    target.append(image_path.split("_")[-2])
+
+
+
+# Get target names
+targetNames = np.unique(target)
+le = LabelEncoder()
+target = le.fit_transform(target)
+
+model = RandomForestClassifier(n_estimators = 33, criterion='entropy')
+
+trainData, testData, trainTarget, testTarget = cross_validation.train_test_split(data, target, test_size=0.4)
+model.fit(trainData, trainTarget)
+print(classification_report(testTarget, model.predict(testData), target_names = targetNames))
+
+def predict(url):
+    global model      
+    # Read image
+    image = io.imread(url)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = cv2.resize(image, (500, 500), interpolation=cv2.INTER_CUBIC)    
+
+    # Use otsu to mask
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    mask = cv2.medianBlur(mask, 5)
+
+    features = describe(image, mask)
+
+    state = le.inverse_transform(model.predict([features]))[0]
+    return {'type': state}    
